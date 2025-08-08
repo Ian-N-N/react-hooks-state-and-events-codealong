36aff8dda1e551882df41eab85feb30526a565f5
Ian Njuguna <ian.njuguna@student.moringaschool.com>
2025-08-08 10:10:44 +0300

    code along hooks state and events

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((prev) => !prev);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  // Change body background whenever `isOn` changes
+  useEffect(() => {
+    document.body.style.backgroundColor = isOn ? "orange" : "darkgrey";
+  }, [isOn]);
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
